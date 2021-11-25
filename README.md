aa2d5df1446a827f89004dc718c34c48a2afd31a
= <=>
2021-11-26 00:41:22 +0700

    first comimit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # PTUDW---Online-Auction---2021
+
+## Install dependencies package
+
+### `npm install` to
---
+++ server.js
@@ -0,0 +1,16 @@
+import 'dotenv/config';
+// dotenv.config();
+import express from 'express';
+
+
+import config from './config/config.js';
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(config.PORT, () => {
+    console.log(`Example app listening at http://localhost:${config.PORT}`)
+})
