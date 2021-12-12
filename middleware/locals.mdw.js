ba61c4492b1f4b3813e0efc26fb5c69b4d313f57
NguyenThaiSon931 <63856850+NguyenThaiSon931@users.noreply.github.com>
2021-12-12 20:44:20 +0700

    login signup about

--- middleware/locals.mdw.js
+++ middleware/locals.mdw.js
@@ -1,5 +1,17 @@
 export default (app) => {
     app.get('/', (req, res) => {
         res.render('home');
-    });
+    }),
+
+    app.get('/login', (req,res)=>{
+        res.render('login');
+    }),
+
+    app.get('/signup', (req,res)=>{
+        res.render('signup');
+    }),
+
+    app.get('/about', (req,res)=>{
+        res.render('about');
+    })
 }
