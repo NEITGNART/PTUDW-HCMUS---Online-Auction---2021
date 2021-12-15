124e62cedb188c2bb461383117e9636f5acee637
= <=>
2021-12-15 10:14:01 +0700

    up

--- server.js
+++ server.js
@@ -11,13 +11,12 @@ import active_view_middleware from './middleware/view.mdw.js';
 import active_route_middleware from './middleware/routes.mdw.js';
 
 const app = express()
-app.use('/public', express.static('public'));
 
 app.use(morgan('dev'))
 app.use(express.urlencoded({
     extended: true
 }));
-
+app.use(express.static('public'));
 
 // app.use(passport.initialize());
 // app.use(passport.session());
