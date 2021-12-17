2001ceef95cf9049a7ff6e24df3da08303be368b
Pham Tien <19127576@student.hcmus.edu.vn>
2021-12-17 18:11:02 +0700

    Loading products from database to locals middleware

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,7 +1,32 @@
 import Product from '../models/product.model.js';
+import moment from 'moment'
 
 export default {
-    index(req, res) {
+    listProduct(req, res) {
         res.render('product');
-    }
+    },
+
+    maskInfo(value) {
+        let maskedValue = value;
+        if (value && value.length > 5) {
+            maskedValue =
+                "***" + maskedValue.substring(value.length - 4, value.length);
+        } else {
+            maskedValue = "****";
+        }
+        return maskedValue;
+    },
+
+    isExpired(date) {
+        return moment(date).diff(moment());
+    },
+
+    extendExpire(date) {
+        return moment(date).add(10, 'minutes');
+    },
+
+    // listProduct(req, res) {
+
+    // }
+
 };
--- middleware/locals.mdw.js
+++ middleware/locals.mdw.js
@@ -1,3 +1,7 @@
+import productController from '../controllers/product.controller.js'
+import CategoryModel from '../models/category.model.js'
+import ProductModel from '../models/product.model.js'
+
 export default (app) => {
     app.use(async function (req, res, next) {
         try {
@@ -12,4 +16,38 @@ export default (app) => {
         }
         next();
     });
+
+    app.use(async (req, res, next) => {
+
+        const products = await ProductModel.find({ status: "bidding" });
+        // update product
+
+        products.forEach(async (product) => {
+
+            var doc = await ProductModel.findOne({ _id: product._id });
+            if (doc.extend === 'yes' && productController.isExpired(product.expDate) <= 5 * 60 * 1000) {
+                doc.extend = 'no';
+                doc.expDate = productController.extendExpire(product.expDate);
+            }
+            if (productController.isExpired(product.expDate) <= 0) {
+                doc.status = 'done';
+                // Update category
+                try {
+                    const cat = await CategoryModel.findOne({ subCate: { $in: doc.category[0] } });
+                    // await cat.save();
+                }
+                catch {
+                    console.log("Error");
+                }
+                // await doc.save();
+            }
+        })
+
+        const cats = await CategoryModel.find();
+        res.locals.cats = cats;
+        res.locals.products = products;
+        next();
+    });
+
+
 }
