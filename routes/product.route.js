ba1eb752b467c0b3d119de38b78dc94861720286
Pham Tien <19127576@student.hcmus.edu.vn>
2022-01-01 11:57:06 +0700

    Pagination product

--- routes/product.route.js
+++ routes/product.route.js
@@ -4,13 +4,7 @@ import controller from '../controllers/product.controller.js';
 const router = express.Router();
 
 router.route('/')
-    .get(controller.listProduct);
-
-router.route('/sort=price')
-    .get(controller.sortProductByPrice);
-
-router.route('/sort=time')
-    .get(controller.sortProductByTime)
+    .get(controller.index);
 
 
 
