3e90eb6a9726aa0b8e43728845a3f77ba75be3ac
Nguyen Thai Son <95063137+nts931ag@users.noreply.github.com>
2022-01-06 22:47:08 +0700

    update home page and create admin page

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -2,4 +2,9 @@ import express from 'express';
 
 const router = express.Router();
 
+router.route('/')
+    .get((req, res) => {
+        res.render('admin',{layout: false});
+    });
+
 export default router;
