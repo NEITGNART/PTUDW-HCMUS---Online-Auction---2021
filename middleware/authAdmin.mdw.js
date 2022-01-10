371ba66ed2dfe54355a2c2c99a737356ca1bf20f
gotoSleep <az9thongnguyen@gmail.com>
2022-01-10 16:46:10 +0700

    reup

--- middleware/authAdmin.mdw.js
+++ middleware/authAdmin.mdw.js
@@ -1,5 +1,6 @@
 export default function auth(req, res, next) {
     if (req.isAuthenticated() && req.user.type === 'admin') {
+        req.session.retUrl = req.originalUrl;
         return next();
     }
     res.redirect(`/login?retUrl=${req.originalUrl}`);
--- middleware/authSeller.mdw.js
+++ middleware/authSeller.mdw.js
@@ -1,5 +1,6 @@
 export default function auth(req, res, next) {
     if (req.isAuthenticated() && req.user.type === 'seller') {
+        req.session.retUrl = req.originalUrl;
         return next();
     }
     res.redirect(`/login?retUrl=${req.originalUrl}`);
