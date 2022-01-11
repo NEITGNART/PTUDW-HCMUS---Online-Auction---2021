36bcc356886adc16d1a8198b110c4b6e727e7e08
Pham Tien <19127576@student.hcmus.edu.vn>
2022-01-11 22:35:54 +0700

    Send email feature

--- config/transporter.js
+++ config/transporter.js
@@ -1,6 +1,27 @@
 import config from './config.js';
 import nodemailer from 'nodemailer';
 
+
+
+// let mailOptions = {
+//     from: config.EMAIL_USER,
+//     to: 'zzmanhtienzz@gmail.com ',
+//     subject: 'hello Tien',
+//     text: 'Hello Tien'
+// }
+
+
+// transporter.sendMail(mailOptions, function(error, info){
+//     if (error) {
+//         console.log(error);
+//     } else {
+//         console.log('Email sent: ' + info.response);
+//     }
+// });
+
+
+
+
 export default nodemailer.createTransport({
     service: 'gmail',
     secure: false,
